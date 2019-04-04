b00301435d4893ccffd910bf05fefa5e835dd3fd
naiyar2000 <49275611+naiyar2000@users.noreply.github.com>
2019-04-04 17:30:34 +0530

    Add files via upload

---
+++ ni.cpp
@@ -0,0 +1,27 @@
+	#include<stdio.h>
+	int main()
+	{
+	int i,j,T,K,N,sum=0;
+	short A,B;
+	scanf("%d",&T);
+	for(i=0;i<15,i<T;i++)
+	{
+	scanf("%d",&N);
+	scanf("%d",&A);
+	scanf("%d",&B);
+	scanf("%d",&K);
+	for(j=1;j<=N;j++)
+	{
+	if(j%A==0&&j%B!=0)
+	sum+=1;
+	if(j%A!=0&&j%B==0)
+	sum+=1;
+	}
+	
+	if(sum>=K)
+	printf("Win");
+	else
+	printf("Lose");
+	return 0;
+    }
+}
